06a19c9546a19ef8f2bcd69161437e663b1a7944
Wiktor Pabich <pabichwiktor@gmail.com>
2022-10-03 23:01:12 +0200

    chore: scrapping interval revert

--- scrapper/const/options.py
+++ scrapper/const/options.py
@@ -1,4 +1,4 @@
 '''options module'''
 QUEUE_BROWSING_INTERVAL = 60 * 15
-SCRAPPING_INTERVAL_SECONDS = 1 * 60  # 1 min
+SCRAPPING_INTERVAL_SECONDS = 8 * 60 * 60  # 8 hours
 SLEEP_AFTER_SEND = 60 * 60  # 60 minutes # TURNED OFF for now
