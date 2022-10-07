1892d7bdc76b444cc46b90212f94fa827fbc002d
Wiktor Pabich <pabichwiktor@gmail.com>
2022-10-07 18:03:46 +0200

    fix(api): cors etc

--- api/index.ts
+++ api/index.ts
@@ -8,6 +8,13 @@ const port = process.env.PORT;
 
 initRoutes();
 
+app.use(function(_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at ${port}`);
 });
--- api/routes/products.ts
+++ api/routes/products.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express'
+import bodyParser from 'body-parser';
 import app from '../__app';
 import db from '../db';
 import { validateToken } from '../utils/tokens';
 import { Token } from '../models/Token';
+
+const jsonParser = bodyParser.json()
 
 export default (): void => {
     app.get('/products', async (req: Request, res: Response): Promise<void> => {
@@ -12,29 +15,31 @@ export default (): void => {
         res.send({ status: 200, data: { product }});
     });
 
-    app.post('/products/watch', async (req: Request<{}, {}, {}, {url: string, threshold_price: string, token: string, email: string}>, res: Response): Promise<void> => {
-        const token: Token = { id: req.query.token }
+    app.post('/products/watch', jsonParser, async (req: Request<{}, {}, {url: string, threshold_price: string, token: string, email: string}>, res: Response): Promise<void> => {
+        const { body } = req;
+
+        const token: Token = { id: body.token }
         const validationResult = await validateToken(token);
+
         if (!validationResult.status) {
             res.send({ status: 401, error: { msg: 'Unauthorized' }});
             return;
         }
 
-        const { query } = req;
         const productsCollection = db.collection('products_queue')
 
-        const findResult = await productsCollection.countDocuments({ 'url': query.url }, { limit: 1 });
+        const findResult = await productsCollection.countDocuments({ 'url': body.url }, { limit: 1 });
         if (findResult > 0) {
-            await productsCollection.updateOne({ url: query.url }, { $addToSet: { recipients: query.email }})
+            await productsCollection.updateOne({ url: body.url }, { $addToSet: { recipients: body.email }})
         } else {
             const insertResult = await productsCollection.insertOne({ 
-                url: query.url, 
-                threshold_price: Number(query.threshold_price),
-                recipients: [query.email]
+                url: body.url, 
+                threshold_price: Number(body.threshold_price),
+                recipients: [body.email]
             })
     
             if (insertResult) {
-                console.log(`[Queue] Product added: ${query.url} : ${query.threshold_price}`)
+                console.log(`[Queue] Product added: ${body.url} : ${body.threshold_price}`)
             }
         }
 
