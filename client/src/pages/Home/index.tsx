1c6102e9be1dc5ca7b61bdbcd5062691152e591d
Wiktor Pabich <pabichwiktor@gmail.com>
2022-12-14 21:49:16 +0100

    fix(style): mobile padding

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ export default function HomePage(): ReactElement {
 	return (
 		<>
 			<Head title='Pricyfy' />
-			<div className='m-2 max-w-[55rem] flex flex-col mx-auto items-center justify-center '>
+			<div className='p-2 max-w-[55rem] flex flex-col mx-auto items-center justify-center '>
 				<Promo />
 				<div className='my-12 -space-y-px rounded-md'>
 					<ProductsShowcase type={ProductsShowcaseTypes.RECENT} />
